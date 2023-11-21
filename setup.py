5ed4ec684b45ab7b99014ee1f1be55451001d62b
Omar Aflak <aflakomar@gmail.com>
2023-11-21 23:37:33 +0000

    specify packages manually in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
@@ -8,5 +8,5 @@ setup(
     author='Omar Aflak',
     author_email='aflakomar@gmail.com',
     description='A tool to coordinate NAT hole punching',
-    packages=find_packages(),
+    packages=['natpunch'],
 )
