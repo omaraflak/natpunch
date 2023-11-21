2750d3562a4d9aa2a36d1b3086160a3855e6ee7c
Omar Aflak <aflakomar@gmail.com>
2023-11-21 23:30:27 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='natpunch',
+    version='1.0.0',
+    url='https://github.com/omaraflak/nat-punch.git',
+    author='Omar Aflak',
+    author_email='aflakomar@gmail.com',
+    description='A tool to coordinate NAT hole punching',
+    packages=find_packages(),
+)
